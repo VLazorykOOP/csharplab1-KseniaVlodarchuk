d867d78b90c67c383a4f07b790b0763e968889a7
KseniaVlodarchuk <161445910+KseniaVlodarchuk@users.noreply.github.com>
2024-02-28 18:52:53 +0200

    add task 1.1

--- Lab1Charp/Program.cs
+++ Lab1Charp/Program.cs
@@ -14,3 +14,35 @@
 Console.WriteLine("Lab 1 !");
 task1();
 // continue ...
+
+ 
+static void Main()
+{
+    Console.WriteLine("Input the first term of the geometric progression:");
+    double firstTerm = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Input the denominator of the geometric progression:");
+    double commonRatio = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Enter the number of members of the geometric progression:");
+    int numberOfTerms = Convert.ToInt32(Console.ReadLine());
+
+    double sum = CalculateGeometricProgressionSum(firstTerm, commonRatio, numberOfTerms);
+
+    Console.WriteLine($"Sum {numberOfTerms} members of the geometric progression: {sum}");
+}
+
+static double CalculateGeometricProgressionSum(double firstTerm, double commonRatio, int numberOfTerms)
+{
+    // Перевірка на те, чи знаменник не дорівнює 1, щоб уникнути ділення на нуль
+    if (commonRatio == 1)
+    {
+        Console.WriteLine("The denominator cannot be equal to 1 in a geometric progression");
+        return double.NaN; // Повертаємо не число у випадку помилки
+    }
+
+    // Використовуємо формулу суми геометричної прогресії
+    double sum = firstTerm * (1 - Math.Pow(commonRatio, numberOfTerms)) / (1 - commonRatio);
+    return sum;
+}
+
